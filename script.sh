3e6375acb17e7ae4b23d64c178aa81f631975c0b
Hostrider <rumyancev-2001@mail.ru>
2022-06-04 13:13:20 +1000

    first commit

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hello, $name, how are you?"
+if [[ ! -f $file_name ]]
+	then
+		touch $file_name
+		echo "$name, file $file_name was created"
+	else
+		echo "$name, file $file_name already consists, if it was created more than 5 days ago he will not be written:"
+		find $file_name  -mtime -5
+fi
